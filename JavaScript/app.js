62b2c9abd7a4068863e6ed78cc783dd62392dfb4
Aman <abhriguvansh99@gmail.com>
2020-04-21 20:25:10 -0700

    added method to find artist from reddit post

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,5 +1,12 @@
-const api = `https://www.reddit.com/r/hiphopheads.json`;
-fetch(api)
+const reddit = `https://www.reddit.com/r/hiphopheads.json`;
+const spotify = `https://api.spotify.com/v1/search`;
+const auth = {
+  headers: {
+    Authorization: `Bearer BQC9O7F2JoMh9qb_Nclz9ZbDODBKTlTK2blBSEaocGHjvzilzVlPsX0noFWW48nxqzKUYPXdg51E09Q74Up8f5VHhVDeV6ifOT5NVHMURXeymcCVEeDrEgaKhOeToRalSK8iZM1Nx154plmV73kQv7aGyXk`,
+  },
+};
+
+fetch(reddit)
   .then((response) => {
     return response.json();
   })
@@ -10,6 +17,33 @@ fetch(api)
       postTitles[i] = posts[i].data.title; //array full of post names
     }
     for (let i = 0; i < posts.length; i++) {
-      console.log(postTitles[i].substring(0, 7));
+      if (postTitles[i].substring(0, 7) == '[FRESH]') {
+        console.log(postTitles[i]);
+        let artist = artistFromTitle(postTitles[i]);
+        console.log(artist);
+        addToSongs();
+      }
     }
   });
+
+//takes in title of reddit post and converts it so we can add it to spotify link to search
+function artistFromTitle(title) {
+  let artist;
+  let string = title.substring(7, title.length);
+  artist = string.replace('-', 'DELETETHIS');
+  let index = artist.search('DELETETHIS');
+  artist = artist.substring(0, index);
+  //artist = artist.replace('DELETETHIS', '');
+  return artist;
+}
+
+//finds song in spotify, add it to page
+function addToSongs() {
+  fetch(spotify, auth)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+}
