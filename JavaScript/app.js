5d5deb9f5cc8835105a454262d293970b59cec3d
Aman <abhriguvansh99@gmail.com>
2020-05-22 09:22:32 -0700

    updated js code to authorize spotify user so that the website can use an auth token to get data from spotify api

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,11 +1,33 @@
 const reddit = `https://www.reddit.com/r/hiphopheads.json`;
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 let spotify = `https://api.spotify.com/v1/search`;
-const auth = {
-  headers: {
-    Authorization: `Bearer BQCavN_EFq-cBcMUiz1QTiazNpD2kUenVRmrQI4zy_JK5WUwG-w8pxu8dlk2d7cV7SzSVxlF6C6z1MAc8kWFhVjGd7Wfo2ap21ChW228RhXEhfdOEkFYboF2MqjLZTR2ADt11WZhdhgWeWOlOO49eutod6U`,
-  },
-};
+
+const hash = window.location.hash
+  .substring(1)
+  .split('&')
+  .reduce(function (initial, item) {
+    if (item) {
+      var parts = item.split('=');
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+    }
+    return initial;
+  }, {});
+window.location.hash = '';
+
+let _token = hash.access_token;
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = '920d39201a3249ffac54403cf1cd069c';
+const redirectUri = 'http://127.0.0.1:5500/index.html';
+const scopes = ['user-top-read'];
+
+if (!_token) {
+  window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    '%20'
+  )}&response_type=token&show_dialog=true`;
+}
 
 const songList = document.querySelector('.song-list');
 
@@ -35,7 +57,7 @@ fetch(reddit)
 function artistFromTitle(title) {
   let artist;
   let string = title.substring(7, title.length);
-  artist = string.replace('-', 'DELETETHIS');
+  artist = string.replace(' - ', 'DELETETHIS');
   let index = artist.search('DELETETHIS');
   artist = artist.substring(0, index);
   artist = artist.replace(' ', '');
@@ -77,12 +99,14 @@ function trackNameFromTitle(title) {
 
 //finds song in spotify, add it to page
 function addToSongs(track, artist) {
-  spotify = `${proxy}https://api.spotify.com/v1/search?q=${track}%20${artist}&type=track`;
-  fetch(spotify, auth)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
+  $.ajax({
+    url: `${proxy}https://api.spotify.com/v1/search?q=${track}%20${artist}&type=track`,
+    type: 'GET',
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader('Authorization', 'Bearer ' + _token);
+    },
+    success: function (data) {
+      // Do something with the returned data
       console.log(data);
       let uri;
       let results = data.tracks.items;
@@ -92,7 +116,8 @@ function addToSongs(track, artist) {
         uri = data.tracks.items[0].uri;
       }
       uri = uri.replace('spotify:track:', '');
-      console.log(uri);
+      let songDiv = document.createElement('div');
+      songDiv.className = 'song';
       let songPlayer = document.createElement('iframe');
       songPlayer.src = `https://open.spotify.com/embed/track/${uri}`;
       songPlayer.width = '300';
@@ -100,6 +125,36 @@ function addToSongs(track, artist) {
       songPlayer.frameBorder = '0';
       songPlayer.allowtransparency = 'true';
       songPlayer.allow = 'encrypted-media';
-      songList.appendChild(songPlayer);
-    });
+      songDiv.appendChild(songPlayer);
+      songList.appendChild(songDiv);
+    },
+  });
+  // spotify = `https://api.spotify.com/v1/search?q=${track}%20${artist}&type=track`;
+  // fetch(spotify)
+  //   .then((response) => {
+  //     console.log(response);
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //     let uri;
+  //     let results = data.tracks.items;
+  //     if (results.length == 0) {
+  //       return;
+  //     } else {
+  //       uri = data.tracks.items[0].uri;
+  //     }
+  //     uri = uri.replace('spotify:track:', '');
+  //     let songDiv = document.createElement('div');
+  //     songDiv.className = 'song';
+  //     let songPlayer = document.createElement('iframe');
+  //     songPlayer.src = `https://open.spotify.com/embed/track/${uri}`;
+  //     songPlayer.width = '300';
+  //     songPlayer.height = '380';
+  //     songPlayer.frameBorder = '0';
+  //     songPlayer.allowtransparency = 'true';
+  //     songPlayer.allow = 'encrypted-media';
+  //     songDiv.appendChild(songPlayer);
+  //     songList.appendChild(songDiv);
+  //   });
 }
